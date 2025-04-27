47881872419c13b20c25454e9f6b63215d3e2a7f
Gaurang-bit786 <sgaurang500@gmail.com>
2025-04-27 10:11:33 +0530

    Commit Code

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import json
 import pickle
 import pandas as pd 
+from random import random
 from flask import Flask, request, render_template
 
 
@@ -29,11 +30,26 @@ common_data = {
 
 fp = open('../model/random_forest_model.pkl', 'rb')
 model = pickle.load(fp)
-print(model.predict([[11,2022,0,7,0,38,2]]))
+fp.close()
+
+df = pd.read_csv("../final_merged_dataset (1).csv")
+
+@app.route('/', methods=['GET'])
+def dashboard():
+    print(df[:2].to_json(orient='records'))
+    return render_template('dashboard.html',common_data={
+        "dist_length":len(common_data['district'].keys()),
+        "var_length":len(common_data['variety'].keys()),
+        "market_length":len(common_data['market'].keys()),
+        "comm_length":len(common_data['commodity'].keys()),
+        "grade_length":len(common_data['grade'].keys()),
+        "data":df[:30].to_dict(orient='records')
+    })
 
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
+
+@app.route('/prediction', methods=['GET', 'POST'])
+def pridect():
     if request.method == 'POST':
         prediction_data = []
         values = list(dict(request.form).values())
@@ -45,8 +61,31 @@ def home():
         )
         prediction_data.extend(values[1:])
         common_data['predict'] = model.predict([prediction_data])[0]
-        return render_template('index.html',common_data=common_data)
-    return render_template('index.html',common_data=common_data)
+        return render_template('prediction.html',common_data=common_data)
+    return render_template('prediction.html',common_data=common_data)
+
+
+@app.route('/create-record', methods=['GET', 'POST'])
+def creat_data():
+    if request.method == 'POST':
+        print(request.form)
+        df = pd.DataFrame({
+            "District Name":request.form.get("district"),
+            "Market Name":request.form.get("market"),
+            "Commodity":request.form.get("commodity"),
+            "Variety":request.form.get("variety"),
+            "Grade":request.form.get("grade"),
+            "Min Price (Rs./Quintal)":request.form.get("min_price"),
+            "Max Price (Rs./Quintal)":request.form.get("max_price"),
+            "Modal Price (Rs./Quintal)":request.form.get("modal_price"),
+            "Price Date":request.form.get("date"),
+            "Temperature":request.form.get("tempt")
+        })
+        print(df)
+        df.to_csv('../final_merged_dataset (1).csv',mode='a', header=False, index=False)
+        return render_template('create-data.html',common_data=common_data)
+    return render_template('create-data.html',common_data=common_data)
+
 
 
 if __name__=="__main__":
